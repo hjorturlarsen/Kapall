c6ddff96a0cbd99b261f5ca7664386fdb07207a1
gudni <gudnithor4@gmail.com>
2014-03-25 23:26:25 +0000

    meira

--- readData.py
+++ readData.py
@@ -11,15 +11,33 @@ def databaseCall():
 	row = c.fetchall()
 
 	# Birtir top 15
-	count = 0
+	count = 1
+	strengur = ""
+	totalString = ""
 	for member in row:
-		print member[0], member[1], member[2]
+		if len(str(member[1])) == 5:
+			length = "    "
+		if len(str(member[1])) == 4:
+			length = "     "
+		if len(str(member[1])) == 3:
+			length = "      "
+		if len(str(member[1])) == 2:
+			length = "       "
+		if len(str(count)) == 1:
+			length2 = ".  "
+		if len(str(count)) == 2:
+			length2 = ". "
+		strengur = str(count) + length2 + str(member[0]) + "    " + str(member[1]) + length + str(member[2]) + "\n"
+		totalString += strengur
 		count += 1
-		if count == 15:
+		if count == 16:
 			break
+	f = open('highscore.txt', 'w')
+	f.write(totalString)
 
 	# Commit before closing
 	conn.commit()
 	# Always close after we finish
 	conn.close()
+
 databaseCall()
