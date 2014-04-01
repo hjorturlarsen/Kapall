4a1cc5d2a831cd83a435c8390bc5d17ef5fbdfef
Hjörtur Larsen <91hjortur@gmail.com>
2014-04-01 22:20:13 +0000

    Score og timi í falli, komment í kóða...

--- GUI.py
+++ GUI.py
@@ -15,67 +15,37 @@ import HighScoreInsertion
 
 class GUI:
 	def __init__(self):
-
-		self.the_timeSec = 0.0
-		self.the_timeMin = 0.0
-		self.the_score = 0
-		score_multiplier = 0
-
 		pygame.init()
 		self.screen = pygame.display.set_mode((800, 500))
 		pygame.display.set_caption('Gooby plz')
 		pygame.mouse.set_visible(1)
 		background = pygame.image.load("data/dolanbackground.png")
 		backgroundRect = background.get_rect()
+		self.font = pygame.font.Font(None, 36)
 
-		self.enter_highscore = False
+		self.the_timeSec = 0.0
+		self.the_timeMin = 0.0
+		self.the_score = 0
+		self.score_multiplier = 0
+		self.highscore_submitted = False
+		self.old_pos = None
 
 		self.game = Golf_relaxed()
-		
 		self.collumns = [self.game.col1.sprites(), self.game.col2.sprites(), self.game.col3.sprites(), self.game.col4.sprites(), self.game.col5.sprites(), self.game.col6.sprites(), self.game.col7.sprites()]
 		allsprites =  pygame.sprite.LayeredUpdates((self.game.col1, self.game.col2, self.game.col3, self.game.col4, self.game.col5, self.game.col6, self.game.col7))
-		allsprites.clear(self.screen, background)
-
-		#Old position of card
-		self.old_pos = None
 
 		clock = pygame.time.Clock()
 
-		# MENU
 		ourMenu = ["Start"]
-
 		myMenu = menu.Menu(ourMenu, 'data/dolanbackground.png')
 		myMenu.drawMenu()
 
-		# Display score
-		font = pygame.font.Font(None, 36)
-		text = font.render("Score: %d" % (self.the_score), 1, (255, 255, 255))
-		textpos = text.get_rect()
-		textpos.center = (680, 475)
-		self.screen.blit(text, textpos)
-
-		# Display time
-		self.start = tm.time()
-		#textTime = font.render("Time: %.0f : %.0f" % (self.the_timeMin, self.the_timeSec), 1, (255, 255, 255))
-		#textposTime = textTime.get_rect()
-		#textposTime.center = (100, 475)
-		#self.screen.blit(textTime, textposTime)
-
 		self.MouseLPressed = False
-		# MAINLOOP
-		while 1:
-			#Timi
-			end = tm.time()
-			tm.sleep(0.01)
-			self.the_timeSec = end-self.start
-			self.the_timeMin = int(self.the_timeSec/60)
-			the_timeSec2 = int(self.the_timeSec % 60)
-			textTime = font.render("Time: %.0f : %.0f" % (self.the_timeMin, the_timeSec2), 1, (255, 255, 255))
 
+		while 1:
 		# INPUT EVENTS
 			for event in pygame.event.get():
 				myMenu.handleEvent(event)
-
 				# QUIT TO MENU
 				if event.type == QUIT:
 					return
@@ -84,18 +54,16 @@ class GUI:
 				elif event.type == menu.Menu.MENUCLICKEDEVENT:
 					#START AND INITIALIZE GAME
 					if event.item == 0:
-						self.start = tm.time()
-						textposTime = textTime.get_rect()
-						textposTime.center = (150, 475)
 						isGameActive = True
 						myMenu.deactivate()
 
+						self.start = tm.time()
 						self.set_up_collumns()
 						self.set_up_deckA()
 						self.set_up_deckB()
 						self.selectable_collumns()
+						self.add_score(0)
 
-			self.screen.blit(background, (0, 0))
 			if myMenu.isActive():
 				myMenu.drawMenu()
 
@@ -103,22 +71,20 @@ class GUI:
 			else:
 				self.screen.blit(background, backgroundRect)
 				allsprites.draw(self.screen)
-				#pygame.display.update(rects)
 				self.update()
 
 				if event.type == MOUSEBUTTONDOWN:
 					self.MouseLPressed = True
+
+					#Draw from deck A
 					for idx, card in enumerate(self.game.deckA.sprites()):
 						if card.clicked(event.pos):
 							if len(self.game.deckA.sprites()) > 0:
 								self.game.deckB.add(card)
 								self.game.deckA.remove(card)
+							self.add_score(50)
 
-							# 50 points if clicked on deck A
-							# and multiplier given the value 0
-							self.the_score += 50
-							score_multiplier = 0
-							text = font.render("Score: %d" % (self.the_score), 1, (255, 255, 255))
+					#Select card from collumn
 					for idx, col in enumerate(self.collumns):
 						for idx2, card in enumerate(col):
 							if card.selectable:
@@ -126,35 +92,38 @@ class GUI:
 
 				if event.type == MOUSEBUTTONUP:
 					self.MouseLPressed = False
+
 					for idx, col in enumerate(self.collumns):
 						for idx2, card in enumerate(col):
 							if card.selected:
 								card.selected = False
 
-								#Collision Detection
+								#Check if card from collumn collides with the last card in Deck B
 								last_in_deckB = self.game.deckB.sprites()[-1]
 								if pygame.sprite.collide_rect(card, last_in_deckB) and (last_in_deckB.child == int(card.rank) or last_in_deckB.parent == int(card.rank)):
 									self.game.deckB.add(card)
+									#remove card from collumn
 									col.pop()
+									#Make last cards in collumns selectable
 									self.selectable_collumns()
 									
 									# Each tima a player can remove more
 									# than 1 card from the board in a row the
 									# score will be multiplied be a higher number
-									score_multiplier += 1
-									self.the_score += 100 + mathh.pow(score_multiplier, 4)
-									text = font.render("Score: %d" % (self.the_score), 1, (255, 255, 255))
-									# elif loops are for the wildcard
+									self.score_multiplier += 1
+									self.add_score(100+mathh.pow(self.score_multiplier, 4))
+
+								#For the wildcard
 								elif pygame.sprite.collide_rect(card, last_in_deckB) and last_in_deckB.id == 'W21':
 									col.pop()
 									self.selectable_collumns()
 									self.game.deckB.add(card)
-									self.the_score += 5000
-									text = font.render("Score: %d" % (self.the_score), 1, (255, 255, 255))
+									self.add_score(5000)
 								elif pygame.sprite.collide_rect(card, last_in_deckB) and card.id == 'W21':
 									col.pop()
 									self.selectable_collumns()
 									self.game.deckB.add(card)
+								#move card to it's original position
 								else:
 									card.rect.x = self.old_pos[0]
 									card.rect.y = self.old_pos[1]
@@ -162,20 +131,14 @@ class GUI:
 					if self.check_for_loss():
 						print "tapadi"
 					if self.check_for_win():
-						print "Winner winner chicken dinner"
 						time = str(self.the_timeMin) + " : " + str(int(self.the_timeSec%60))
-						print "Time: " + time + "\n" + "Score: " + str(self.the_score)
-						if self.enter_highscore == False:
+						if self.highscore_submitted == False:
 							HighScoreInsertion.insertHighscore(randrange(10000000) , self.ask(self.screen, "Name: "), str(self.the_score), time)
-							self.enter_highscore = True
+							self.highscore_submitted = True
 
 
 					self.set_up_deckB()
 					self.set_up_deckA()
-
-				#Update time and score
-				self.screen.blit(text, textpos)
-				self.screen.blit(textTime, textposTime)
 
 				if self.MouseLPressed == True:
 					for idx, col in enumerate(self.collumns):
@@ -202,6 +165,10 @@ class GUI:
 			card.draw(self.screen)
 		for idx, card in enumerate(deckB):
 			card.draw(self.screen)
+		self.update_time()
+		self.screen.blit(self.text, self.textpos)
+		self.screen.blit(self.textTime, self.textposTime)
+		self.screen.blit(self.text, self.textpos)
 
 	def set_up_collumns(self):
 		x = 50
@@ -295,3 +262,19 @@ class GUI:
 			return True
 		else:
 			return False
+
+	def update_time(self):
+		end = tm.time()
+		tm.sleep(0.01)
+		self.the_timeSec = end-self.start
+		self.the_timeMin = int(self.the_timeSec/60)
+		the_timeSec2 = int(self.the_timeSec % 60)
+		self.textTime = self.font.render("Time: %.0f : %.0f" % (self.the_timeMin, the_timeSec2), 1, (255, 255, 255))
+		self.textposTime = self.textTime.get_rect()
+		self.textposTime.center = (150, 475)
+
+	def add_score(self, number):
+		self.the_score += number
+		self.text = self.font.render("Score: %d" % (self.the_score), 1, (255, 255, 255))
+		self.textpos = self.text.get_rect()
+		self.textpos.center = (680, 475)
